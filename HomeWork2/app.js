0ed6965ff65a4224105371da45543e38560a6704
grynevych89 <grynevych89@gmail.com>
2022-08-27 17:00:51 +0300

    add home work3

--- HomeWork2/app.js
+++ HomeWork2/app.js
@@ -1,4 +1,4 @@
 const answerOne = prompt('Введите первое число');
 const answerTwo = prompt('Введите второе число');
-let result = Number(answerOne) + Number(answerTwo)
+const result = Number(answerOne) + Number(answerTwo)
 alert(answerOne + " + " + answerTwo + " = " + result);
---
+++ HomeWork3/app.js
@@ -0,0 +1,36 @@
+let numberOne, numberTwo, chooseOperand, result;
+
+do {
+    numberOne = prompt('Введите первое число');
+} while (isInvalid(numberOne));
+
+do {
+    chooseOperand = prompt('Введите действие: +, -, * или /');
+} while (isSymvol(chooseOperand));
+
+do {
+    numberTwo = prompt('Введите второе число');
+} while (isInvalid(numberTwo));
+
+switch (chooseOperand) {
+    case '+' : result = numberOne + numberTwo; break;
+    case '-' : result = numberOne - numberTwo; break;
+    case '*' : result = numberOne * numberTwo; break;
+    case '/' : result = numberOne / numberTwo; break;
+}
+
+alert(numberOne + ' ' + chooseOperand + ' ' + numberTwo + ' = ' + result);
+
+function isInvalid(str) {
+    return str === null || str.trim() === '' || isNaN(str);
+};
+
+function isSymvol(str) {
+    switch(str) {
+        case '+': return false;
+        case '-': return false;
+        case '*': return false;
+        case '=': return false;
+        default: return true;
+    };
+};
